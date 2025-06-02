501f25a6a7d76c928b82c60fb6ae11248da334b3
Esteban Cortina <ecortina@devteam.hmhsistemas.com.mx>
2025-06-01 22:35:03 -0600

    Add vuetify

---
+++ src/views/home-view.vue
@@ -0,0 +1,15 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+import HelloWorld from '../components/hello-world.vue';
+
+export default {
+  name: 'home-view',
+
+  components: {
+    HelloWorld,
+  },
+};
+</script>
