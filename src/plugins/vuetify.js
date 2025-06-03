59d0294acf2c247f930a3072214169997eabed06
Esteban Cortina <ecortina@devteam.hmhsistemas.com.mx>
2025-06-02 20:27:52 -0600

    Add project identity

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,23 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#0f5731',
+        secondary: '#f5f4f1',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+      },
+      dark: {
+        primary: colors.blue.lighten3,
+      },
+    },
+  },
 });
