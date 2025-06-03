f6c376ccb488eb2fe63f5a74ebff3f5a0c866fef
Esteban Cortina <ecortina@devteam.hmhsistemas.com.mx>
2025-06-02 22:11:40 -0600

    Add fetch patients & update

--- src/views/patients-view.vue
+++ src/views/patients-view.vue
@@ -1,3 +1,16 @@
 <template>
-  <div><h1>Pacientes</h1></div>
+  <v-patients-cards-com>
+    <div><h1>Pacientes</h1></div>
+  </v-patients-cards-com>
 </template>
+
+<script>
+import PatientsCardsCom from '@/components/patients-cards-com.vue';
+
+export default {
+  name: 'PatientsView',
+  components: {
+    'v-patients-cards-com': PatientsCardsCom,
+  },
+};
+</script>
